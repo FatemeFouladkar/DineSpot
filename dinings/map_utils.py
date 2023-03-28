e45edd3759e7543497f4865bc49c3c8c61476057
fatemefouladkar <fatemeflk@chmail.ir>
2023-03-28 15:11:17 +0330

    add markers to the map

---
+++ dinings/map_utils.py
@@ -0,0 +1,25 @@
+from django.template.loader import render_to_string
+
+import folium
+import branca 
+
+from .models import Dining, Link, Image
+
+
+def make_popup_data(dining):
+    context = {
+          'dining': dining
+    }
+    return render_to_string('popup_data.html', context=context)
+
+
+def make_markers_and_add_to_map(map, popup_data, dining):
+        iframe = branca.element.IFrame(html=popup_data, width=300, height=400)
+        popup = folium.Popup(iframe, max_width=300)
+        
+        folium.Marker(
+            location = [dining.latitude, dining.longitude],
+            popup=popup,
+            tooltip = dining.name,
+            icon = folium.Icon(icon='fa-coffee', prefix='fa')
+        ).add_to(map)
--- dinings/views.py
+++ dinings/views.py
@@ -1,8 +1,32 @@
 from django.views.generic import TemplateView
 
+import folium
+from folium.plugins import LocateControl, Search
+
+from .models import Dining
+from .map_utils import make_markers_and_add_to_map, make_popup_data
 
 
 class MapView(TemplateView):
     template_name = 'map.html'    
 
+    def get_context_data(self, **kwargs):
+        figure = folium.Figure()
+        map = folium.Map(
+            location = [36.2971, 59.5953],
+            zoom_start = 13,
+            tiles = 'OpenStreetMap',
+            control_scale=True,
+            max_bounds=True
+        )
+        map.add_to(figure)
+        
+        for dining in Dining.objects.all():
+            popup_data = make_popup_data(dining)
+            make_markers_and_add_to_map(map, popup_data, dining)
 
+        LocateControl().add_to(map)
+
+        figure.render()
+        return {"map": figure}
+    
