e8ec94023ba4bb210ca4ecdd8fcec604a6ddafc5
fatemefouladkar <fatemeflk@chmail.ir>
2023-04-25 12:42:58 +0330

    fix PointField and __str__

--- dinings/models.py
+++ dinings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.gis.db import models as gis_models
-
+from django.contrib.gis.geos import Point
 from .utils import dining_image_upload_path
 
 
@@ -9,18 +9,18 @@ class Dining(models.Model):
     description = models.TextField(null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
     phone_number = models.CharField(max_length=100, null=True, blank=True)
-    location = gis_models.PointField(srid=4326, null=True, blank=True)
+    location = gis_models.PointField(srid=4326, null=True, blank=True, default=Point(36.2971, 59.5953))
     
     @property
     def latitude(self):
-        return self.location.coords[1]
+        return self.location.coords[1] if self.location else 36.2971
 
     @property
     def longitude(self):
-        return self.location.coords[0]
+        return self.location.coords[0] if self.location else 59.5953
     
     def __str__(self) -> str:
-        return self.name
+        return self.name if self.name else self.pk
     
 
 
@@ -30,7 +30,7 @@ class Link(models.Model):
     dining = models.ForeignKey(Dining, on_delete=models.CASCADE, related_name='links')
 
     def __str__(self) -> str:
-        return self.key
+        return self.key if self.key else self.pk
 
 
 class Image(models.Model):
